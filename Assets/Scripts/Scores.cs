753a1e61d59eaf28af51976a9930d0ff48f24536
Your Name <you@example.com>
2023-05-05 13:28:27 +0300

    Step-2

---
+++ Assets/Scripts/Scores.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scores : MonoBehaviour
+{
+    public Text scoreText;
+    private int _currentScore;
+    void Start()
+    {
+        _currentScore = 0;
+        scoreText.text = _currentScore.ToString();
+    }
+
+    public void AddScore()
+    {
+        _currentScore += 10;
+        scoreText.text = _currentScore.ToString();
+    }
+
+    public void DeductScore()
+    {
+        //if koşulunun tek satır hali, 0 dan büyükse 10 azalt değilse 0 yaz
+        _currentScore = _currentScore > 0 ? _currentScore - 10 : 0;
+        scoreText.text = _currentScore.ToString();
+    }
+
+}
